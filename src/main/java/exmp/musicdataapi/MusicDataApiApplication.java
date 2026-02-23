7c9fac3d9f98584dd5771873f5968238d8833307
alanymarinho <amcoelho@uolinc.com>
2026-02-23 16:14:08 -0300

    Criando projeto

---
+++ src/main/java/exmp/musicdataapi/MusicDataApiApplication.java
@@ -0,0 +1,13 @@
+package exmp.musicdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicDataApiApplication.class, args);
+    }
+
+}
